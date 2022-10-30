2895422365fc7178060ecae1ed070121884aa0b4
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-10-29 22:44:19 -0600

    clave encriptada

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,13 @@
 import User from "../models/User.js"
+import bcrypt from 'bcrypt';
 
 export const createUser = async (req, res) => {
   try {
-    const user = await User.create(req.body);
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8)
+    });
     res.status(201).json({
       "message": "User Created",
       "id": user.id
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 import  express from "express";
 
-import {
-  createUser
-} from "../controllers/user.js";
+import {createUser}  from "../controllers/user.js";
+import { login } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post('/users', createUser);
+
+router.post('/auth', login)
 
 export default router;
