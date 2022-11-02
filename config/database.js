f581f8a6da00d671c0de530c346875e25bc1f9d9
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-11-02 15:57:35 -0700

    actualizcion

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD, {
-  host: "localhost",
+  host: process.env.DB_HOST,
   dialect: "postgres",
   logging: false,
   port: 5432,
