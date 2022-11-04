fda0b984c42a2d0fc1a595d27368d8d1f91ee6dd
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-11-03 23:03:59 -0700

    actualizando

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+  process.env.DB_NAME || 'user2' ,
+  process.env.DB_USER || '123456' ,
+  process.env.DB_PASSWORD || 'postgres_prueba' , {
+  host: process.env.DB_HOST || '127.0.0.1' ,
   dialect: "postgres",
   logging: false,
   port: 5432,
